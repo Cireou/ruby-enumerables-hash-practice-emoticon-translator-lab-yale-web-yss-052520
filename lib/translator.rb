5084ac5eec484312b5d475a47e6308484a73c689
Eric  Ou <eric.ou@yale.edu>
2020-05-07 05:51:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,9 @@
-# require modules here
+require "pry"
+require "yaml"
 
 def load_library
-  # code goes here
+  file = YAML.load("emoticons.yml")
+  binding.pry
 end
 
 def get_japanese_emoticon
@@ -11,3 +13,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+binding.pry
