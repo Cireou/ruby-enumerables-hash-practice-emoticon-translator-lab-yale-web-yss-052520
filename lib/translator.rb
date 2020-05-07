bc6a0d84ac19e0f39ef4bca7f54b4582231b05e1
Eric  Ou <eric.ou@yale.edu>
2020-05-07 06:23:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,11 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emote)
-  # hash = load_library(path)
-  nil
+  hash = load_library(path)
+  hash.each do |des, lang_hash|
+    return hash[des][:japanese] if hash[des][:english] == emote
+  end 
+  puts "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emote)
@@ -20,4 +23,5 @@ def get_english_meaning(path, emote)
   hash.each do |des, lang_hash| 
     return des if lang_hash[:japanese] == emote
   end 
+  puts "Sorry, that emoticon was not found"
 end
